38bd03392b0f3bf7f50d4bf620d5ea841e0a56c6
hosung-222 <62132755+hosung-222@users.noreply.github.com>
2023-02-19 23:33:43 +0900

    unique mapping

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne // 자식 입장에서 부모 하나
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+    joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+    inverseJoinColumns = @JoinColumn(name = "ITEM_ID"))
+    private List<ITEM> items = new ArrayList<>();
+
+}
