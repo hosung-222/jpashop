3b5b4d59eb89bb31c557367675b96ca5dc340127
hosung-222 <gik11kr@naver.com>
2023-06-29 19:52:29 +0900

    add item business logic

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,8 +2,9 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
@@ -27,5 +28,24 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories  = new ArrayList<>();
 
+    // 비즈니스 로직 //
+    /**
+     * 재고 증가
+     * @param quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
 
+    /**
+     * 재고 감소
+     * @param quantity
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
